e3f2185b3435782ea328e7886717d2f70f9bfa77
Joshua Herghelegiu-Liu <sirherg@gmail.com>
2020-06-08 01:54:57 -0700

    Spawning UI elements with Unity 3D Camera
    This adddon is for those who wish to spawn 2D UI elements onto a canvas without using an Orthographic
    2D camera. This is a limitation in the Unity engine, and this is a workaround to produce the same effect
    with the 3D camera.

---
+++ UIElement/ReadMe.txt
@@ -0,0 +1,3 @@
+This adddon is for those who wish to spawn 2D UI elements onto a canvas without using an Orthographic
+2D camera. This is a limitation in the Unity engine, and this is a workaround to produce the same effect
+with the 3D camera. 
---
+++ UIElement/UI Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISpawner : MonoBehaviour
+{
+    public GameObject UIElement;
+
+
+    void start() { }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0 && Time.timeScale == 1.0f)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = new Vector3(touch.position.x, touch.position.y, 0);
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+            RaycastHit hitResult;
+            Physics.Raycast(ray, out hitResult);
+            Vector3 textPos = hitResult.point;
+            if (touch.phase == TouchPhase.Began)
+                Instantiate(UIElement, textPos, Quaternion.identity);
+        }
+    }
+}
